36e1b8d402a8e044481ac6348c40706b1b996e44
Alan Lengkoan <alanlengkoan15@gmail.com>
2025-08-09 23:12:15 +0800

    update again

--- application/views/admin/consultation/js/result.php
+++ application/views/admin/consultation/js/result.php
@@ -1,17 +1,27 @@
 <script src="https://d3js.org/d3.v7.min.js"></script>
 <script>
-    // Data pohon dari PHP
+    // Data pohon dari PHP (pastikan format: { name: "root", children: [...] })
     var treeData = <?= $tree_json ?>;
 
-    // Set ukuran
+    // Hitung jumlah node untuk menentukan tinggi dinamis
+    function countNodes(node) {
+        let count = 1;
+        if (node.children) {
+            node.children.forEach(c => count += countNodes(c));
+        }
+        return count;
+    }
+    var totalNodes = countNodes(treeData);
+    var nodeSpacing = 40; // jarak antar node
+    var height = totalNodes * nodeSpacing;
+
     var margin = {
             top: 20,
             right: 90,
             bottom: 30,
-            left: 90
+            left: 150
         },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        width = 1200 - margin.left - margin.right;
 
     var svg = d3.select("#tree-container").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -20,14 +30,8 @@
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     var treemap = d3.tree().size([height, width]);
-    var root = d3.hierarchy(treeData, function(d) {
-        return d.children;
-    });
+    var root = d3.hierarchy(treeData);
 
-    root.x0 = height / 2;
-    root.y0 = 0;
-
-    // Generate tree layout
     root = treemap(root);
 
     // Add links
@@ -39,35 +43,24 @@
         .attr("stroke", "#ccc")
         .attr("stroke-width", 2)
         .attr("d", d3.linkHorizontal()
-            .x(function(d) {
-                return d.y;
-            })
-            .y(function(d) {
-                return d.x;
-            }));
+            .x(d => d.y)
+            .y(d => d.x));
 
     // Add nodes
     var node = svg.selectAll(".node")
         .data(root.descendants())
         .enter().append("g")
         .attr("class", "node")
-        .attr("transform", function(d) {
-            return "translate(" + d.y + "," + d.x + ")";
-        });
+        .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
 
     node.append("circle")
-        .attr("r", 6)
+        .attr("r", 5)
         .attr("fill", "#69b3a2");
 
     node.append("text")
         .attr("dy", ".35em")
-        .attr("x", function(d) {
-            return d.children ? -10 : 10;
-        })
-        .attr("text-anchor", function(d) {
-            return d.children ? "end" : "start";
-        })
-        .text(function(d) {
-            return d.data.name;
-        });
+        .attr("x", d => d.children ? -12 : 12)
+        .attr("text-anchor", d => d.children ? "end" : "start")
+        .style("font-size", "12px")
+        .text(d => d.data.name);
 </script>
